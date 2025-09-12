98f9582b1931c33898fb98ee0f9f09099dcb9765
ralfhartmann1994-hue <ralfhartmann1994@gmail.com>
2025-09-12 20:57:40 +0100

    Profile_manger.py

--- Profile_manger.py
+++ Profile_manger.py
@@ -10,25 +10,25 @@ def ensure_user(uid: int):
     return ensure_user_storage(uid)
 
 def set_name(user_id, name):
-    storage.USERS.setdefault(user_id, {})
-    storage.USERS[user_id]["name"] = name
+    storage.users.setdefault(user_id, {})
+    storage.users[user_id]["name"] = name
 
 def get_name(user_id):
-    return storage.USERS.get(user_id, {}).get("name")
+    return storage.users.get(user_id, {}).get("name")
 
 def set_gender(user_id, gender):
-    storage.USERS.setdefault(user_id, {})
-    storage.USERS[user_id]["gender"] = gender
+    storage.users.setdefault(user_id, {})
+    storage.users[user_id]["gender"] = gender
 
 def get_gender(user_id):
-    return storage.USERS.get(user_id, {}).get("gender")
+    return storage.users.get(user_id, {}).get("gender")
 
 def set_age(user_id, age):
-    storage.USERS.setdefault(user_id, {})
-    storage.USERS[user_id]["age"] = age
+    storage.users.setdefault(user_id, {})
+    storage.users[user_id]["age"] = age
 
 def get_age(user_id):
-    return storage.USERS.get(user_id, {}).get("age")
+    return storage.users.get(user_id, {}).get("age")
 
 def profile_text(u: dict):
     name = u.get("name") or "غير محدد"
@@ -49,4 +49,4 @@ def end_session(u1: dict, u2: dict=None):
             u2["partner"] = None
             u2["chat_started_at"] = None
     except Exception:
-        pass
+        pass 
